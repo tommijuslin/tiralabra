defbb682c79a74a9bfc0b4a96566d698be5e7a63
tommijuslin <tommi.juslin@gmail.com>
2022-12-10 15:23:33 +0200

    remove useless print

--- src/ui/ohjelma.py
+++ src/ui/ohjelma.py
@@ -2,78 +2,78 @@ import re
 from colorama import Fore
 
 KOMENNOT = {
-  "1:": "korjaa sana tai lause",
-  "2:": "lisää sana",
-  "q:": "lopeta",
+    "1:": "korjaa sana tai lause",
+    "2:": "lisää sana",
+    "q:": "lopeta",
 }
 
+
 class Ohjelma():
-  """Pääohjelma."""
+    """Pääohjelma."""
 
-  def __init__(self, io, levenshtein, lauseenkorjaaja):
-    """Luokan konstruktori.
+    def __init__(self, io, levenshtein, lauseenkorjaaja):
+        """Luokan konstruktori.
 
-    Parametrit:
-      io: lukemiseen ja tulostamiseen käytettävä apuluokka
-      lauseenkorjaaja: lauseen korjaamiseen käytettävä luokka
-    """
+        Parametrit:
+          io: lukemiseen ja tulostamiseen käytettävä apuluokka
+          lauseenkorjaaja: lauseen korjaamiseen käytettävä luokka
+        """
 
-    self._io = io
-    self._levenshtein = levenshtein
-    self._lauseenkorjaaja = lauseenkorjaaja
+        self._io = io
+        self._levenshtein = levenshtein
+        self._lauseenkorjaaja = lauseenkorjaaja
 
-  def kaynnista(self):
-    """Ohjelman pääsilmukka.
-       Lukee syötteitä ja kutsuu metodeja.
-    """
+    def kaynnista(self):
+        """Ohjelman pääsilmukka.
+           Lukee syötteitä ja kutsuu metodeja.
+        """
 
-    while True:
-      self._tulosta_ohje()
-      komento = self._io.lue("> ")
+        while True:
+            self._tulosta_ohje()
+            komento = self._io.lue("> ")
 
-      if komento == "q":
-        break
-      if komento == "1":
-        self._korjaa_lause()
-      elif komento == "2":
-        self._lisaa_sana()
+            if komento == "q":
+                break
+            if komento == "1":
+                self._korjaa_lause()
+            elif komento == "2":
+                self._lisaa_sana()
 
-  def _tulosta_ohje(self):
-    """Tulostaa käyttöohjeet."""
+    def _tulosta_ohje(self):
+        """Tulostaa käyttöohjeet."""
 
-    self._io.tulosta(24*"=")
-    for komento, kuvaus in KOMENNOT.items():
-      print(komento, kuvaus)
-    self._io.tulosta(24*"=")
+        self._io.tulosta(24*"=")
+        for komento, kuvaus in KOMENNOT.items():
+            print(komento, kuvaus)
+        self._io.tulosta(24*"=")
 
-  def _korjaa_lause(self):
-    """Korjaa syötteen kirjoitusvirheet."""
+    def _korjaa_lause(self):
+        """Korjaa syötteen kirjoitusvirheet."""
 
-    self._io.tulosta("Syötä sana tai lause (tyhjä lopettaa):")
+        self._io.tulosta("Syötä sana tai lause (tyhjä lopettaa):")
 
-    while True:
-      lause = re.findall(r"\w+|[^\w\s]", self._io.lue("> ").lower())
-      print(lause)
-      if not lause:
-        break
-      korjattu_lause = self._lauseenkorjaaja.korjaa(lause)
-      self._io.tulosta(f"==> {self._muotoile_tulostus(korjattu_lause)}")
+        while True:
+            lause = re.findall(r"\w+|[^\w\s]", self._io.lue("> ").lower())
+            if not lause:
+                break
+            korjattu_lause = self._lauseenkorjaaja.korjaa(lause)
+            self._io.tulosta(f"==> {self._muotoile_tulostus(korjattu_lause)}")
 
-  def _lisaa_sana(self):
-    """Lisää sanan sanakirjaan"""
+    def _lisaa_sana(self):
+        """Lisää sanan sanakirjaan"""
 
-    sana = self._io.lue("sana: ")
-    self._levenshtein.lisaa(sana)
+        sana = self._io.lue("sana: ")
+        self._levenshtein.lisaa(sana)
 
-  def _muotoile_tulostus(self, lause):
-    tulos = ""
-    for sana in lause:
-      if sana[-1] == 0:
-        tulos += f"{sana[0]} "
-      else:
-        tulos += f"{Fore.GREEN}{sana[0]}{Fore.RESET} "
+    def _muotoile_tulostus(self, lause):
+        tulos = ""
+        for sana in lause:
+            if sana[-1] == 0:
+                tulos += f"{sana[0]} "
+            else:
+                tulos += f"{Fore.GREEN}{sana[0]}{Fore.RESET} "
 
-    # välimerkkien kanssa kikkailua
-    tulos = re.sub(r' (?=[.?!,])', '', tulos)
+        # välimerkkien kanssa kikkailua
+        tulos = re.sub(r' (?=[.?!,])', '', tulos)
 
-    return tulos
+        return tulos
